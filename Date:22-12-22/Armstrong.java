bca209f90a7531d651a05011bd242e9b87e8491e
Rajat Dey <120620768+Rajat985@users.noreply.github.com>
2022-12-23 13:59:40 +0530

    Create Armstrong.java

---
+++ Date:22-12-22/Armstrong.java
@@ -0,0 +1,37 @@
+package Basic;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		int num, temp, totalDigit=0, res=0, rem;
+	      Scanner scan = new Scanner(System.in);
+	      
+	      System.out.print("Enter the Number: ");
+	      num = scan.nextInt();
+	      
+	      temp = num;
+	      while(num>0)
+	      {
+	         num /= 10;
+	         totalDigit++;
+	      }
+	      
+	      num = temp;
+	      while(num>0)
+	      {
+	         rem = num%10;
+	         res = res + (int) Math.pow(rem, totalDigit);
+	         num /= 10;
+	      }
+	      
+	      if(res==temp)
+	         System.out.println("\nArmstrong Number");
+	      else
+	         System.out.println("\nNot an Armstrong Number.");
+	}
+
+}
